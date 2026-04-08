742089f4b5606701ad3ec49c36c7cd5fd6e17941
ValPQT <valentinpasquiet@gmail.com>
2026-04-08 16:29:17 +0200

    feat : add files for the Task2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that provides a floor function for float numbers."""
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float n as an integer."""
+    return math.floor(n)
