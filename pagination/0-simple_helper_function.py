b38e211c97e34390cf8075643046964e80e4f205
ValPQT <valentinpasquiet@gmail.com>
2026-04-21 16:14:44 +0200

    fix : update for pycodestyle

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,8 +4,6 @@
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     """Return the start and end indexes corresponding to a pagination range."""
-
     start = (page - 1) * page_size
     end = page * page_size
-
     return (start, end)
