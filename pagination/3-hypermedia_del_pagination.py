06c6ab2b987e1cc84c277a869a80f549525f344e
ValPQT <valentinpasquiet@gmail.com>
2026-04-22 15:21:57 +0200

    feat : update files for Task3

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Return the cached dataset loaded from the CSV file."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Return dataset indexed by position, starting at index 0."""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None,
+                        page_size: int = 10) -> Dict:
+        """Return a page of data resilient to deletions."""
+        assert index is not None and isinstance(index, int) and index >= 0
+
+        dataset = self.indexed_dataset()
+        assert index < len(dataset)
+
+        data = []
+        current_index = index
+
+        while len(data) < page_size and current_index < len(dataset):
+            if current_index in dataset:
+                data.append(dataset[current_index])
+            current_index += 1
+
+        return {
+            "index": index,
+            "data": data,
+            "page_size": len(data),
+            "next_index": current_index
+        }
