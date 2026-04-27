4d4dfe3fbb09ffc56f2c96b93cf72ba278f88ff7
ValPQT <valentinpasquiet@gmail.com>
2026-04-27 15:55:59 +0200

    feat : add files for Task 0

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,12 @@
+function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+function taskNext() {
+  let task = 'But sometimes let is okay';
+  task = 'Updated value';
+  return task;
+}
+
+export { taskFirst, taskNext };
