80ea8be4fe6d702f03be877282fa968b0d7c3e7c
ValPQT <valentinpasquiet@gmail.com>
2026-04-28 13:55:41 +0200

    fix : update the output

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,11 +1,15 @@
-function taskFirst() {
+export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-function taskNext() {
-  let task = 'But sometimes let is okay';
-  return task;
+export function getLast() {
+  return ' is okay';
 }
 
-export { taskFirst, taskNext };
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
